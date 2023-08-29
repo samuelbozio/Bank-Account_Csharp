6f3739c929e2836d3d75cb7966b92e3fd58ec532
Samuel Bozio <samuelbozio@hotmail.com>
2023-08-29 20:32:19 -0300

    exercicio conta bancaria em csharp

---
+++ EXERCÍCIO-1/Conta.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXERCÍCIO_1
+{
+    internal class Conta
+    {
+
+        public int numero_conta { get; }
+        public string nome_titular { get; private set; }
+        public double saldo { get; private set; }
+        public double deposito_inicial { get; private set; }
+
+        public Conta(int numero_conta, string nome_titular, double saldo)
+        {
+            this.numero_conta = numero_conta;
+            this.nome_titular = nome_titular;
+            this.saldo = saldo;
+            
+        }
+
+    
+
+        public void Saque(double valor)
+        {
+            if (saldo >= (valor + 5))
+            {
+                saldo -= valor;
+            }
+            else
+            {
+                Console.WriteLine("Saldo insuficiente para saque.");
+            
+            }
+        }
+
+        public void Deposito(double valor)
+        {
+            saldo += valor;
+           
+        }
+
+        public override string ToString()
+        {
+            return numero_conta +  ", " + nome_titular + ", " + saldo;
+        }
+
+    }
+
+    }
+
---
+++ EXERCÍCIO-1/Program.cs
@@ -0,0 +1,42 @@
+﻿using EXERCÍCIO_1;
+using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Conta conta;
+            Console.WriteLine("CADASTRO DE CONTAS: ");
+            Console.WriteLine("Entre com o número da conta: ");
+            int numero_conta = int.Parse(Console.ReadLine());
+            Console.WriteLine("Entre com o nome do titular: ");
+            string nome_titular = Console.ReadLine();
+            Console.WriteLine("Haverá depósito inicial? [S/N] ");
+            char resposta = char.Parse(Console.ReadLine());
+            if (resposta == 'S')
+            {
+                Console.WriteLine("Informe o depósito inicial: ");
+                double dep_inicial = double.Parse(Console.ReadLine());
+                conta = new Conta(numero_conta, nome_titular, dep_inicial); 
+               
+            } else
+            {
+                conta = new Conta(numero_conta, nome_titular, 0.0);
+            }
+            
+            Console.WriteLine(conta.ToString());
+            conta.Saque(100);
+            Console.WriteLine(conta.ToString());
+            conta.Deposito(100);
+            Console.WriteLine(conta.ToString());
+
+
+
+
+        }
+    }
+}
